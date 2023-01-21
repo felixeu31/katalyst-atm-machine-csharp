16e78176ce32118be754d82b7c62dd16c932b106
Félix Díez <felixeu31@gmail.com>
2023-01-21 22:20:48 +0100

    test: failing withdraw_one_coin_of_two_euros

--- AtmMachine/AtmMachine.Tests/AtmMachineTests.cs
+++ AtmMachine/AtmMachine.Tests/AtmMachineTests.cs
@@ -8,7 +8,7 @@ namespace AtmMachine.Tests
         }
 
         [Test]
-        public void withdraw_one_coin_of_one_euro()
+        public void withdraw_one_coin_of_one_euros()
         {
             // Arrange
             AtmMachine atmMachine = new();
@@ -18,6 +18,20 @@ namespace AtmMachine.Tests
 
             // Assert
             money.Value.Should().Be(1);
+            money.Type.Should().Be(MoneyType.Coin);
+        }
+
+        [Test]
+        public void withdraw_one_coin_of_two_euros()
+        {
+            // Arrange
+            AtmMachine atmMachine = new();
+
+            // Act
+            var money = atmMachine.Withdraw(2);
+
+            // Assert
+            money.Value.Should().Be(2);
             money.Type.Should().Be(MoneyType.Coin);
         }
     }
