bccb66704e1f32f6f472b4de0c2a00998831184a
Félix Díez <felixeu31@gmail.com>
2023-01-21 22:08:04 +0100

    test: failing withdraw_one_coin_of_one_euro

--- AtmMachine/AtmMachine.Tests/AtmMachineTests.cs
+++ AtmMachine/AtmMachine.Tests/AtmMachineTests.cs
@@ -8,9 +8,33 @@ namespace AtmMachine.Tests
         }
 
         [Test]
-        public void Test1()
+        public void withdraw_one_coin_of_one_euro()
         {
-            true.Should().BeTrue();
+            // Arrange
+            AtmMachine atmMachine = new();
+
+            // Act
+            var money = atmMachine.Withdraw(1);
+
+            // Assert
+            money.Value.Should().Be(1);
+            money.Type.Should().Be("Coin");
         }
+
+
+    }
+
+    public class AtmMachine
+    {
+        public Money Withdraw(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Money
+    {
+        public int Value { get; set; }
+        public string Type { get; set; }
     }
 }
