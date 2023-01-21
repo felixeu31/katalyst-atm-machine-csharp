cfcaacce9f403f6af9fee3a2a26ded8f0722284d
Félix Díez <felixeu31@gmail.com>
2023-01-21 21:59:48 +0100

    chore: initial proyect

---
+++ AtmMachine/AtmMachine.Tests/AtmMachineTests.cs
@@ -0,0 +1,16 @@
+namespace AtmMachine.Tests
+{
+    public class AtmMachineTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
